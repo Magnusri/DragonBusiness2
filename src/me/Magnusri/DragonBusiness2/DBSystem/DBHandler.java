90f2da5d785696c2ead4ee99ec6da0d4911865c5
Magnusri <magnus_rydjord@hotmail.com>
2014-12-02 21:10:26 +0100

    DB Example implemented, and jdbc included

--- src/me/Magnusri/DragonBusiness2/DBSystem/DBHandler.java
+++ src/me/Magnusri/DragonBusiness2/DBSystem/DBHandler.java
@@ -1,5 +1,14 @@
 package me.Magnusri.DragonBusiness2.DBSystem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+
 public class DBHandler {
 	private DBConnection conn;
 	
@@ -8,4 +17,114 @@ public class DBHandler {
 		
 		
 	}
+	
+	private Connection connect = null;
+	  private Statement statement = null;
+	  private PreparedStatement preparedStatement = null;
+	  private ResultSet resultSet = null;
+
+	  public void readDataBase() throws Exception {
+	    try {
+	      // this will load the MySQL driver, each DB has its own driver
+	      Class.forName("com.mysql.jdbc.Driver");
+	      // setup the connection with the DB.
+	      connect = DriverManager
+	          .getConnection("jdbc:mysql://localhost/feedback?"
+	              + "user=sqluser&password=sqluserpw");
+
+	      // statements allow to issue SQL queries to the database
+	      statement = connect.createStatement();
+	      // resultSet gets the result of the SQL query
+	      resultSet = statement
+	          .executeQuery("select * from FEEDBACK.COMMENTS");
+	      writeResultSet(resultSet);
+
+	      // preparedStatements can use variables and are more efficient
+	      preparedStatement = connect
+	          .prepareStatement("insert into  FEEDBACK.COMMENTS values (default, ?, ?, ?, ? , ?, ?)");
+	      // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
+	      // parameters start with 1
+	      preparedStatement.setString(1, "Test");
+	      preparedStatement.setString(2, "TestEmail");
+	      preparedStatement.setString(3, "TestWebpage");
+	      preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
+	      preparedStatement.setString(5, "TestSummary");
+	      preparedStatement.setString(6, "TestComment");
+	      preparedStatement.executeUpdate();
+
+	      preparedStatement = connect
+	          .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
+	      resultSet = preparedStatement.executeQuery();
+	      writeResultSet(resultSet);
+
+	      // remove again the insert comment
+	      preparedStatement = connect
+	      .prepareStatement("delete from FEEDBACK.COMMENTS where myuser= ? ; ");
+	      preparedStatement.setString(1, "Test");
+	      preparedStatement.executeUpdate();
+	      
+	      resultSet = statement
+	      .executeQuery("select * from FEEDBACK.COMMENTS");
+	      
+	    } catch (Exception e) {
+	      throw e;
+	    } finally {
+	      close();
+	    }
+
+	  }
+
+	  private void writeResultSet(ResultSet resultSet) throws SQLException {
+	    // resultSet is initialised before the first data set
+	    while (resultSet.next()) {
+	      // it is possible to get the columns via name
+	      // also possible to get the columns via the column number
+	      // which starts at 1
+	      // e.g., resultSet.getSTring(2);
+	      String user = resultSet.getString("myuser");
+	      String website = resultSet.getString("webpage");
+	      String summary = resultSet.getString("summary");
+	      Date date = resultSet.getDate("datum");
+	      String comment = resultSet.getString("comments");
+	      System.out.println("User: " + user);
+	      System.out.println("Website: " + website);
+	      System.out.println("Summary: " + summary);
+	      System.out.println("Date: " + date);
+	      System.out.println("Comment: " + comment);
+	    }
+	  }
+
+	  // you need to close all three to make sure
+	  private void close() {
+	    close(resultSet);
+	    close(statement);
+	    close(connect);
+	  }
+	  private void close(ResultSet c) {
+	    try {
+	      if (c != null) {
+	        c.close();
+	      }
+	    } catch (Exception e) {
+	    // don't throw now as it might leave following closables in undefined state
+	    }
+	  }
+	  private void close(Statement c) {
+		    try {
+		      if (c != null) {
+		        c.close();
+		      }
+		    } catch (Exception e) {
+		    // don't throw now as it might leave following closables in undefined state
+		    }
+		  }
+	  private void close(Connection c) {
+		    try {
+		      if (c != null) {
+		        c.close();
+		      }
+		    } catch (Exception e) {
+		    // don't throw now as it might leave following closables in undefined state
+		    }
+		  }
 }

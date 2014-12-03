99d343082e64c4e52bac113a5b079c097b5c7579
Magnusri <magnus_rydjord@hotmail.com>
2014-12-03 22:04:36 +0100

    DB Connected! getPlayerList, insertPlayer working!
    Next: Stop insert if player exists.

--- src/me/Magnusri/DragonBusiness2/DBSystem/DBHandler.java
+++ src/me/Magnusri/DragonBusiness2/DBSystem/DBHandler.java
@@ -6,15 +6,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 
 public class DBHandler {
-	private DBConnection conn;
 	
 	public DBHandler(){
-		conn = new DBConnection();
-		
 		
 	}
 	
@@ -22,76 +22,71 @@ public class DBHandler {
 	  private Statement statement = null;
 	  private PreparedStatement preparedStatement = null;
 	  private ResultSet resultSet = null;
-
-	  public void readDataBase() throws Exception {
-	    try {
-	      // this will load the MySQL driver, each DB has its own driver
-	      Class.forName("com.mysql.jdbc.Driver");
-	      // setup the connection with the DB.
+	  
+	  public boolean insertPlayer(Plugin plugin, Player player, Double balance) throws ClassNotFoundException, SQLException{
+		  
+		  for (DBPlayer dbPlayer : getPlayerList()){
+			  if (dbPlayer.getUuid().equals(player.getUniqueId())){
+				  return false;
+			  }
+		  }
+		  
+		  Class.forName("com.mysql.jdbc.Driver");
+	      
 	      connect = DriverManager
-	          .getConnection("jdbc:mysql://localhost/feedback?"
-	              + "user=sqluser&password=sqluserpw");
-
-	      // statements allow to issue SQL queries to the database
-	      statement = connect.createStatement();
-	      // resultSet gets the result of the SQL query
-	      resultSet = statement
-	          .executeQuery("select * from FEEDBACK.COMMENTS");
-	      writeResultSet(resultSet);
-
-	      // preparedStatements can use variables and are more efficient
+	    		  .getConnection("jdbc:mysql://localhost/dragonbusiness?"
+	    				  + "user=minecraft&password=minecraftpass");
+	      
 	      preparedStatement = connect
-	          .prepareStatement("insert into  FEEDBACK.COMMENTS values (default, ?, ?, ?, ? , ?, ?)");
-	      // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-	      // parameters start with 1
-	      preparedStatement.setString(1, "Test");
-	      preparedStatement.setString(2, "TestEmail");
-	      preparedStatement.setString(3, "TestWebpage");
-	      preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
-	      preparedStatement.setString(5, "TestSummary");
-	      preparedStatement.setString(6, "TestComment");
-	      preparedStatement.executeUpdate();
-
-	      preparedStatement = connect
-	          .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-	      resultSet = preparedStatement.executeQuery();
-	      writeResultSet(resultSet);
-
-	      // remove again the insert comment
-	      preparedStatement = connect
-	      .prepareStatement("delete from FEEDBACK.COMMENTS where myuser= ? ; ");
-	      preparedStatement.setString(1, "Test");
+		          .prepareStatement("INSERT INTO player (player_uuid, player_name, player_rank, player_earned) VALUES (?, ?, 'none', ?)");
+	      
+	      preparedStatement.setString(1, player.getUniqueId().toString());
+	      preparedStatement.setString(2, player.getName());
+	      preparedStatement.setString(3, Double.toString(balance));
+	      
 	      preparedStatement.executeUpdate();
 	      
-	      resultSet = statement
-	      .executeQuery("select * from FEEDBACK.COMMENTS");
-	      
-	    } catch (Exception e) {
-	      throw e;
-	    } finally {
 	      close();
-	    }
+		  return true;
+	  }
+	  public boolean insertPlayer(Plugin plugin, Player player) throws ClassNotFoundException, SQLException{
+		  
+		  insertPlayer(plugin, player, 0.0);
+		  
+		  return true;
+	  }
+	  
+	  public ArrayList<DBPlayer> getPlayerList() throws ClassNotFoundException, SQLException{
+		  
+		  Class.forName("com.mysql.jdbc.Driver");
+		  
+		  connect = DriverManager
+		          .getConnection("jdbc:mysql://localhost/dragonbusiness?"
+		              + "user=minecraft&password=minecraftpass");
 
+	      statement = connect.createStatement();
+	      
+	      resultSet = statement
+	          .executeQuery("select * from player");
+	      ArrayList<DBPlayer> playerList = makePlayerList(resultSet);
+	      close();
+	      return playerList;
 	  }
 
-	  private void writeResultSet(ResultSet resultSet) throws SQLException {
-	    // resultSet is initialised before the first data set
-	    while (resultSet.next()) {
-	      // it is possible to get the columns via name
-	      // also possible to get the columns via the column number
-	      // which starts at 1
-	      // e.g., resultSet.getSTring(2);
-	      String user = resultSet.getString("myuser");
-	      String website = resultSet.getString("webpage");
-	      String summary = resultSet.getString("summary");
-	      Date date = resultSet.getDate("datum");
-	      String comment = resultSet.getString("comments");
-	      System.out.println("User: " + user);
-	      System.out.println("Website: " + website);
-	      System.out.println("Summary: " + summary);
-	      System.out.println("Date: " + date);
-	      System.out.println("Comment: " + comment);
-	    }
+	  private ArrayList<DBPlayer> makePlayerList(ResultSet resultSet) throws SQLException {
+		  	ArrayList<DBPlayer> dbPlayerList = new ArrayList<DBPlayer>();
+		    while (resultSet.next()) {
+		      int id = resultSet.getInt("player_id");
+		      String uuid = resultSet.getString("player_uuid");
+		      String name = resultSet.getString("player_name");
+		      String rank = resultSet.getString("player_rank");
+		      int companyid = resultSet.getInt("company_company_id");
+		      Double earned = resultSet.getDouble("player_earned");
+		      
+		      DBPlayer dbPlayer = new DBPlayer(id, uuid, name, rank, companyid, earned);
+		      dbPlayerList.add(dbPlayer);
+		    }
+		    return dbPlayerList;
 	  }
 
 	  // you need to close all three to make sure
